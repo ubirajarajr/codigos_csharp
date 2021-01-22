d181db15683397fd0992ffdbe00f2160a8d313ad
ubirajarajr <unjunior@hotmail.com>
2021-01-22 15:10:29 -0300

    Programa que informa qual pessoa eh a mais velha

---
+++ Course/Pessoa.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Course
+{
+    class Pessoa
+    {
+        public string Nome;
+        public int Idade;
+
+
+    }
+}
---
+++ Course/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pessoa p = new Pessoa();
+            Console.WriteLine("Dados da primeira pessoa: ");
+            Console.Write("Nome: ");
+            p.Nome = Console.ReadLine();
+            Console.Write("Idade: ");
+            p.Idade = int.Parse(Console.ReadLine());
+
+            Pessoa p1 = new Pessoa();
+            Console.WriteLine("Dados da segunda pessoa: ");
+            Console.Write("Nome: ");
+            p1.Nome = Console.ReadLine();
+            Console.Write("Idade: ");
+            p1.Idade = int.Parse(Console.ReadLine());
+
+            if (p.Idade > p1.Idade)
+            {
+                Console.WriteLine("Pessoa mais velha: " +p.Nome);
+            }
+            else
+            {
+                Console.WriteLine("Pessoa mais velha: " +p1.Nome);
+            }
+
+        }
+    }
+}
